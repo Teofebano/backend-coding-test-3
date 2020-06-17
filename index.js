46f78650ec55cab4d390e946b0725f453316a2bb
archiafinno <archiafinno@gmail.com>
2020-06-17 21:45:56 +0700

    Successfully implement Pagination on endpoint, test, and API Doc

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jsonParser = bodyParser.json();
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
-
+const { logger } = require("./src/helpers/logger");
 const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
@@ -17,5 +17,5 @@ db.serialize(() => {
 
     const app = require('./src/app')(db);
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    app.listen(port, () => logger.info(`date: ${new Date()}, msg: App started and listening on port ${port}`));
 });
